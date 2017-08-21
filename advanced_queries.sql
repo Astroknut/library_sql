2408d4ac3e1bfbe63bc8766fa1df1a356428b27b
Astroknut <charlesbarrett1521@gmail.com>
2017-08-20 21:59:10 -0600

    Finishes Advanced Queries

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -3,16 +3,30 @@
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
 
+SELECT * FROM books INNER JOIN authors ON (authors.id = books.author_id) WHERE authors.name = 'George R.R. Martin';
+
 -- Find all fields (book and author related) for all books written by Milan Kundera.
+
+SELECT * FROM books INNER JOIN authors ON (authors.id = books.author_id) WHERE authors.name = 'Milan Kundera';
 
 -- Find all books written by an author from China or the UK.
 
+SELECT * FROM books INNER JOIN authors ON (authors.id = books.author_id) WHERE authors.nationality = 'China' OR authors.nationality = 'United Kingdom';
+
 -- Find out how many books Albert Camus wrote.
 
+SELECT title FROM books RIGHT OUTER JOIN authors ON (authors.id = books.author_id) WHERE authors.name = 'Albert Camus';
+
 -- Find out how many books written before 1980 were by authors not from the US.
+
+SELECT * FROM books FULL JOIN authors ON (books.author_id = authors.id) WHERE books.publication_date < 1980 AND authors.nationality NOT LIKE 'United States%';
 
 -- For these last two, you should not need a JOIN.
 
 -- Find all authors whose names start with 'J'.
 
+SELECT name FROM authors WHERE name LIKE 'J%';
+
 -- Find all books whose titles contain 'the'.
+
+SELECT title FROM books WHERE title LIKE '%the%';
